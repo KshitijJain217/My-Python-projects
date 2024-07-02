f194a4dda61b475c5a2d6b085d2508ead04df28c
Kshitij Jain <22ad10ks635@mitsgwl.ac.in>
2024-07-02 22:05:15 +0530

    Create Hangman.py

---
+++ Hangman.py
@@ -0,0 +1,111 @@
+
+import random
+
+logo = ''' 
+ _                                             
+| |                                            
+| |__   __ _ _ __   __ _ _ __ ___   __ _ _ __  
+| '_ \ / _` | '_ \ / _` | '_ ` _ \ / _` | '_ \ 
+| | | | (_| | | | | (_| | | | | | | (_| | | | |
+|_| |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|
+                    __/ |                      
+                   |___/    '''
+
+print(logo)
+
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+
+end_of_game = False
+word_list = ["ardvark", "baboon", "camel"]
+chosen_word = random.choice(word_list)
+word_length = len(chosen_word)
+
+lives = 6
+
+print(f'Pssst, the solution is {chosen_word}.')
+
+display = []
+for _ in range(word_length):
+    display += "_"
+
+while not end_of_game:
+    guess = input("Guess a letter: ").lower()
+    if guess in display :
+      print("you've repeated your answer")
+    else:
+      for position in range(word_length):
+          letter = chosen_word[position]
+        # print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
+          if letter == guess:
+              display[position] = letter
+
+      if guess not in chosen_word:
+          lives -= 1
+          print("Wrong guess. Try again!")
+          if lives == 0:
+              end_of_game = True
+              print("You lose.")
+
+      print(f"{' '.join(display)}")
+
+      if "_" not in display:
+          end_of_game = True
+
+          print("You win.")
+
+      print(stages[lives])
