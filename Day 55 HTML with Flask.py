9982afcffd39b0f44160538bdaa0763edee672dc
Kshitij Jain <22ad10ks635@mitsgwl.ac.in>
2026-07-26 18:32:06 +0530

    Update Day 55 HTML with Flask.py

--- Day 55 HTML with Flask.py
+++ Day 55 HTML with Flask.py
@@ -8,15 +8,18 @@ def hello_world():
     return 'Hello, World!'
 
 
+# different routes using the app.route decorator
 @app.route("/bye")
 def say_bye():
     return "Bye"
 
 
+# creating variable paths and converting the path to a specified data type
 @app.route("/username/<path:name>/<int:number>")
 def say_username(name, number):
     return f"hello there {name}, you are {number} years old"
 
 
 if __name__ == '__main__':
+    # run app in debug mode to auto-reload
     app.run(debug=True)
