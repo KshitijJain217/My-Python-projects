f367a0ea88e5d8da629a990670c4ad88d4e39037
Kshitij Jain <22ad10ks635@mitsgwl.ac.in>
2026-07-31 19:53:49 +0530

    Update post.py

--- Day 57 Templating Jinja in Flask Applications/Blog Website capstone Project/post.py
+++ Day 57 Templating Jinja in Flask Applications/Blog Website capstone Project/post.py
@@ -4,3 +4,10 @@ class Post:
         self.title = title
         self.subtitle = subtitle
         self.body = body
+
+# Because the data is stored in object instances rather than standard Python dictionaries, you can access properties
+# using dot notation inside your Jinja HTML templates:
+#
+# With post.py (Objects): {{ post.title }} and {{ post.id }}
+#
+# Without post.py (Raw JSON / Dicts): {{ post['title'] }} and {{ post['id'] }}
