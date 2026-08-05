d9ed63e1528b474b8ad79cb3c443e90dc8c49634
Kshitij Jain <22ad10ks635@mitsgwl.ac.in>
2026-08-05 15:17:05 +0530

    sends an email on form submission

--- Day 59 Blog capstone with Added styling and bootstrap/main.py
+++ Day 59 Blog capstone with Added styling and bootstrap/main.py
@@ -1,11 +1,13 @@
-from flask import Flask, render_template,request
+from flask import Flask, render_template, request
+import smtplib
 import requests
+# made under DAY 60
+app = Flask(__name__)
 
 # USE YOUR OWN npoint LINK! ADD AN IMAGE URL FOR YOUR POST. 👇
 posts = requests.get("https://api.npoint.io/c790b4d5cab58020d391").json()
-
-app = Flask(__name__)
-
+OWN_EMAIL = "YOUR OWN EMAIL ADDRESS"
+OWN_PASSWORD = "YOUR EMAIL ADDRESS PASSWORD"
 
 @app.route('/')
 def get_all_posts():
@@ -21,13 +23,16 @@ def about():
 def contact():
     if request.method == "POST":
         data = request.form
-        print(data["name"])
-        print(data["email"])
-        print(data["phone"])
-        print(data["message"])
+        send_email(data["name"], data["email"], data["phone"], data["message"])
         return render_template("contact.html", msg_sent=True)
     return render_template("contact.html", msg_sent=False)
 
+def send_email(name, email, phone, message):
+    email_message = f"Subject:New Message\n\nName: {name}\nEmail: {email}\nPhone: {phone}\nMessage:{message}"
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(OWN_EMAIL, OWN_PASSWORD)
+        connection.sendmail(OWN_EMAIL, OWN_EMAIL, email_message)
 
 @app.route("/post/<int:index>")
 def show_post(index):
@@ -39,4 +44,4 @@ def show_post(index):
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5001)
+    app.run(debug=True)
