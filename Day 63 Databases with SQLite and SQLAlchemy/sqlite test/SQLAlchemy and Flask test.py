c0ff0eee93c9eb50094f060edbbcce800facb659
Kshitij Jain <22ad10ks635@mitsgwl.ac.in>
2026-08-13 10:15:20 +0530

    Day 63

---
+++ Day 63 Databases with SQLite and SQLAlchemy/sqlite test/SQLAlchemy and Flask test.py
@@ -0,0 +1,7 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Integer,String, Float
+
+
+
