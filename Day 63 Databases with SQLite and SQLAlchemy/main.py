f33383f5b96ca7e86d72a52040fa308f1e12b138
Kshitij Jain <22ad10ks635@mitsgwl.ac.in>
2026-08-15 23:48:56 +0530

    day 63

--- Day 63 Databases with SQLite and SQLAlchemy/main.py
+++ Day 63 Databases with SQLite and SQLAlchemy/main.py
@@ -57,5 +57,21 @@ def add():
     return render_template("add.html")
 
 
+@app.route("/edit", methods=["GET", "POST"])
+def edit():
+    if request.method == "POST":
+        # UPDATE RECORD
+        book_id = request.form.get("book_id")
+        book_to_update = db.get_or_404(Book, book_id)
+        book_to_update.title = request.form["rating"]
+        db.session.commit()
+        return redirect(url_for('home'))
+    book_id = request.args.get("id")
+    book_selected = db.get_or_404(Book, book_id)
+    return render_template("edit.html", book=book_selected)
+
+
 if __name__ == "__main__":
     app.run(debug=True)
+
+    
